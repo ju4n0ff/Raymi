dc0baa87c93ac71e7431e25198326b04a0a367ae
Sebastian <sebastianbelli566@gmail.com>
2026-04-09 19:26:22 -0500

    Implementacion de Types

--- Front-Raymi/src/App.jsx
+++ Front-Raymi/src/App.jsx
@@ -4,11 +4,13 @@ import Cursor from "./Components/Cursor/Cursor.jsx"
 import Navbar from "./Components/Navbar/Navbar.jsx"
 import ContactSection from "./Components/ContactSection/ContactSection.jsx"
 import Footer from "./Components/Footer/Footer.jsx"
+import Hero from "./Components/Hero/Hero.jsx"
 function App() {
   return (
     <>
       <Cursor />
       <Navbar />
+      <Hero/>
       <Footer/>
     </>
   );
