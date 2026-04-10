b94be6e9eb7c95068a7925460b883f71cd064dbd
Sebastian <sebastianbelli566@gmail.com>
2026-04-09 19:10:41 -0500

    Desarrollo de Footer

--- Front-Raymi/src/App.jsx
+++ Front-Raymi/src/App.jsx
@@ -3,13 +3,13 @@ import "./app.css"
 import Cursor from "./Components/Cursor/Cursor.jsx"
 import Navbar from "./Components/Navbar/Navbar.jsx"
 import ContactSection from "./Components/ContactSection/ContactSection.jsx"
-
+import Footer from "./Components/Footer/Footer.jsx"
 function App() {
   return (
     <>
       <Cursor />
       <Navbar />
-      <ContactSection />
+      <Footer/>
     </>
   );
 }
